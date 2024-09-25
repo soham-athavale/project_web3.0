11041b6238a9e65b40a2929af2a660b37feaafca
Soham Athavale <sohamathavale@gmail.com>
2024-09-26 00:00:24 +0530

    Changes to package.json and vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/project_web3.0/' // Replace with your repository name
 })
+
